5c0398111757429b100a245fcbaa023767b85f3b
formantstudio <skepsisdrumandbass@gmail.com>
2025-12-29 06:30:26 -0500

    Updatged part of the page list

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -1,4 +1,4 @@
-from .models import CarMake, CarModel
+from djangoapp.models import CarMake, CarModel
 
 def initiate():
     car_make_data = [
@@ -36,3 +36,5 @@ def initiate():
 
     for data in car_model_data:
             CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
+
+initiate()
