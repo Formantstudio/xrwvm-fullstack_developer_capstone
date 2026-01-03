a3b14722633f4cea52dce5438c4fd8af8828e985
formantstudio <skepsisdrumandbass@gmail.com>
2026-01-02 23:42:31 -0500

    Capstone Lab 4 dynamic pages complete

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,25 +11,22 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
-    params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
-
-    request_url = backend_url+endpoint+"?"+params
-
-    print("GET from {} ".format(request_url))
+    request_url = backend_url.rstrip("/") + "/" + endpoint.lstrip("/")
+    print(f"GET from {request_url} params={kwargs}")
     try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(request_url)
+        response = requests.get(request_url, params=kwargs if kwargs else None)
+        response.raise_for_status()
         return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        return None
+
+
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -37,12 +34,14 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
-# def post_review(data_dict):
+
+
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url.rstrip("/") + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
-        print(response.json())
+        response = requests.post(request_url, json=data_dict)
+        response.raise_for_status()
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        return None
