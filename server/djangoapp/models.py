c2925d47835e2825879aaa177c80a6f795069eeb
Formant <30559290+Formantstudio@users.noreply.github.com>
2026-01-03 00:05:35 -0600

    Refactor validators and format CarModel fields

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,5 @@
-# START models.py 
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CarMake(models.Model):
@@ -14,7 +13,6 @@ class CarMake(models.Model):
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-
     dealer_id = models.IntegerField(default=1)
 
     CAR_TYPES = [
@@ -23,13 +21,19 @@ class CarModel(models.Model):
         ("WAGON", "Wagon"),
     ]
 
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default="SUV"
+    )
 
     year = models.IntegerField(
         default=2023,
-        validators=[MaxValueValidator(2023), MinValueValidator(2015)],
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ],
     )
 
     def __str__(self):
         return f"{self.car_make.name} {self.name}"
-# END models.py 
